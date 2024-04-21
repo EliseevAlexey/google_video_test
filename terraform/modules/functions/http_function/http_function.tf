b0e38f816eaa5711d407fb7be0a67048c21e0bf1
Alexey Eliseev <eliseev.co@gmail.com>
2024-04-20 17:17:06 -0700

    trigger

--- terraform/modules/functions/http_function/http_function.tf
+++ terraform/modules/functions/http_function/http_function.tf
@@ -1,8 +1,8 @@
-resource "google_cloudfunctions_function" "function" {
+resource "google_cloudfunctions_function" "http-function" {
   name                  = "http-function"
-  service_account_name  = "${var.service_account_name}@${var.project_id}.iam.gserviceaccount.com"
+  service_account_name  = var.service_account_email
   trigger_http          = true
   source_archive_bucket = ""
   runtime               = "go122"
-  entry_point           = "HttpFunction"
+  entry_point           = "HelloWorld"
 }
